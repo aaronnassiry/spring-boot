c66bd0670ee92c398c4966f930de5670eeef5f11
Aaron Nassiry <anassiry@salesforce.com>
2023-04-23 14:46:09 -0700

    add @Autowired

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,13 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/v1/students")
 public class StudentController {
+    private final StudentService studentService;
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
     @GetMapping
     public List<Student> getStudents() {
-        return new StudentService().getStudents();
+        return studentService.getStudents();
     }
 }
